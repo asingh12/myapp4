f32fa340ebbc55105e7bf50b7266487478c635ae
Amrinder Singh <asingh@linfield.edu>
2016-05-19 21:40:12 -0700

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,21 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | COMP 405 FINAL App"
+
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | COMP 405 FINAL App"
   end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | COMP 405 FINAL App"
+  end
+
 
 end
