42ce4e43263577757c7ba76d0e3a3d77201413cb
Amrinder Singh <asingh@linfield.edu>
2016-05-20 21:10:57 -0700

    fix view users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,7 +7,7 @@ class UsersControllerTest < ActionController::TestCase
     @other_user = users(:archer)
   end
 
-    test "should redirect index when not logged in" do
+   test "should redirect index when not logged in" do
     get :index
     assert_redirected_to login_url
   end
