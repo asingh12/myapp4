992632cf08d387c1ae6b7feea7db49e998b93bf3
Amrinder Singh <asingh@linfield.edu>
2016-05-23 16:07:14 -0700

    gemfile fix

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,11 +1,11 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
-<<<<<<< HEAD
+
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-=======
-  mount_uploader :picture, PictureUploader
+
+  
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validate  :picture_size
@@ -18,5 +18,5 @@ class Micropost < ActiveRecord::Base
         errors.add(:picture, "should be less than 5MB")
       end
     end
->>>>>>> 250a0a5431064f8a84c90dbc077dd456a4582a14
+
 end
