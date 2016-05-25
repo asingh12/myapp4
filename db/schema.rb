d5db1922b132e2d27e315f4f37b0d2fb9ce30aa0
Amrinder Singh <asingh@linfield.edu>
2016-05-25 15:16:12 -0700

    add school

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160521204612) do
+ActiveRecord::Schema.define(version: 20160525221259) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160521204612) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin",           default: false
+    t.string   "school"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
